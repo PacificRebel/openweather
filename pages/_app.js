32ee7a7fb1a898cf5f3291843b88855c77277725
Heli Sivunen <heli@mooncat.tv>
2021-09-10 15:53:03 +0100

    add custom colours

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,32 @@
 import '../styles/globals.css'
-import { ChakraProvider} from '@chakra-ui/react'
-import theme from '../styles/theme'
+import { ThemeProvider, theme, CSSReset } from '@chakra-ui/react'
+
+const customTheme = {
+    ...theme,
+    colors: {
+      ...theme.colors,
+      spacejelly: "#692ba8",
+      purple: {
+      50: '#f5e9ff',
+      100: '#dac1f3',
+      200: '#c098e7',
+      300: '#a571dc',
+      400: '#8c48d0',
+      500: '#722fb7',
+      600: '#59238f',
+      700: '#3f1968',
+      800: '#260f40',
+      900: '#10031a',
+    }
+  }
+}
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
+    <ThemeProvider theme={customTheme}>
+      <CSSReset />
       <Component {...pageProps} />
-    </ChakraProvider>
+    </ThemeProvider>
   );
 }
 
